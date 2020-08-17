df418f3ab6324e29531ad0642ea0b01009db6f39
MingAo.Wang <490746560@qq.com>
2020-08-17 17:13:16 +0800

    新增宿主和插件之间使用Broadcast通信

--- app/src/main/java/com/optima/plugin/plugin2/Plugin2MainActivity.java
+++ app/src/main/java/com/optima/plugin/plugin2/Plugin2MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.optima.plugin.repluginlib.base.BaseActivity;
+
+public class Plugin2MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
