01a837f04768a9642af18129747d6123e82aefec
MingAo.Wang <490746560@qq.com>
2021-01-22 10:33:33 +0800

    1.修改 gradle 版本，适配 JAVA 1.8

--- app/src/main/java/com/optima/plugin/plugin2/Plugin2MainActivity.java
+++ app/src/main/java/com/optima/plugin/plugin2/Plugin2MainActivity.java
@@ -3,8 +3,12 @@ package com.optima.plugin.plugin2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.optima.plugin.repluginlib.base.BaseActivity;
+
+//import org.webrtc.EglBase;
 
 public class Plugin2MainActivity extends BaseActivity {
 
@@ -12,5 +16,11 @@ public class Plugin2MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View.OnClickListener listener = view -> {
+            Toast.makeText(Plugin2MainActivity.this,"dddddd",Toast.LENGTH_SHORT).show();
+        };
+        findViewById(R.id.btn_test).setOnClickListener(listener);
+//        EglBase eglBase = EglBase.create();
+
     }
 }
